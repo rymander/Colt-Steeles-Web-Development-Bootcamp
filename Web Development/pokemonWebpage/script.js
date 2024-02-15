74fba7dc9db89187b365faa54b9184b22761426a
Ryan <rysuzara@gmail.com>
2024-02-15 09:52:03 -0800

    changed font styles and aligned items

--- Web Development/pokemonWebpage/script.js
+++ Web Development/pokemonWebpage/script.js
@@ -1,9 +1,15 @@
+const container = document.querySelector('#container')
+
 for (let i = 1; i < 252; i++){
     let newImg = document.createElement('img')
+    const newDiv = document.createElement('div')
+    const pokenum = document.createElement('span')
+    pokenum.innerText = `#${i}`
 newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`
-newImg.setAttribute('class', 'sprite')
-let body = document.querySelector('body')
-body.appendChild(newImg)
-
+newDiv.classList.add('pokemon')
+newImg.classList.add('sprite')
+newDiv.appendChild(newImg)
+newDiv.appendChild(pokenum)
+container.appendChild(newDiv)
 }
 
