f25325e68b52473e4c195f837815b51373f87229
Ryan <rysuzara@gmail.com>
2024-04-30 15:16:08 -0700

    completed mongoose/espress CRUD exercises

--- Mongoose_CODE/Mongoose_Express/index.js
+++ Mongoose_CODE/Mongoose_Express/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
+
+
 const Product = require('./models/product');
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -17,10 +20,14 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 app.get('/dog', (req, res) =>{
     res.send('WOOF')
 })
+
+const categories = ['fruit', 'vegetable', 'dairy', 'bread']
 
 
 
@@ -29,11 +36,40 @@ app.get('/products', async (req, res) =>{
     res.render('products/index', { products })
 })
 
+app.get('/products/new', (req, res)=>{
+    res.render('products/new', { categories })
+})
+
+app.post('/products', async (req, res)=>{
+    const newProduct = new Product(req.body)
+    await newProduct.save();
+    console.log(newProduct)
+    res.redirect(`/products/${newProduct._id}`)
+})
+
 app.get('/products/:id', async (req, res) =>{
     const { id } = req.params;
     const product = await Product.findById(id)
     console.log(product)
     res.render('products/show', { product })
+})
+
+app.get('/products/:id/edit', async (req, res)=>{
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    res.render('products/edit', { product, categories })
+})
+
+app.put('/products/:id', async (req, res)=>{
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true})
+    res.redirect(`/products/${product._id}`)
+})
+
+app.delete('/products/:id', async (req, res)=> {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id)
+    res.redirect('/products')
 })
 
 app.listen(3000, () =>{
