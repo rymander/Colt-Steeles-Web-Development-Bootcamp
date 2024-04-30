ada9a91dd945d82dd215c5e6a62cde64012e2887
Ryan <rysuzara@gmail.com>
2024-04-30 08:51:37 -0700

    joining mongoose w/express exercises

---
+++ Mongoose_CODE/Mongoose_Express/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Mongo CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH Mongo Connection NO ERROR!!!!")
+        console.log(err)
+    })
+
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+app.get('/dog', (req, res) =>{
+    res.send('WOOF')
+})
+
+
+
+app.get('/products', async (req, res) =>{ 
+   const products = await Product.find({})
+    res.render('products/index', { products })
+})
+
+app.get('/products/:id', async (req, res) =>{
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    console.log(product)
+    res.render('products/show', { product })
+})
+
+app.listen(3000, () =>{
+    console.log('App is listening on port 3000!')
+})
