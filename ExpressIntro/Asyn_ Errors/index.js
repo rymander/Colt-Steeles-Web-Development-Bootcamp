736ebffffaa2122f0d535756fc4e73b56daebeaf
Ryan <rysuzara@gmail.com>
2024-07-05 14:34:35 -0700

    completed async error handling exercises

--- ExpressIntro/Asyn_ Errors/index.js
+++ ExpressIntro/Asyn_ Errors/index.js
@@ -50,33 +50,52 @@ app.get('/products/new', (req, res)=>{
     res.render('products/new', { categories })
 })
 
-app.post('/products', async (req, res)=>{
+app.post('/products', async (req, res, next)=>{
+    try {
     const newProduct = new Product(req.body)
     await newProduct.save();
     console.log(newProduct)
     res.redirect(`/products/${newProduct._id}`)
+    }
+    catch(e){
+        next(e)
+    }
 })
 
-app.get('/products/:id', async (req, res, next) =>{
+function wrapAsync(fn){
+    return function(req,res,next){
+        fn(req, res, next).catch(e => next(e))
+    }
+}
+
+app.get('/products/:id', wrapAsync(async (req, res, next) =>{ //checks to see if product is found, runs AppError if not
+
     const { id } = req.params;
     const product = await Product.findById(id)
     if(!product){
-       return next(new AppError('Product not found', 404))
+       throw new AppError('Product not found!', 404)
     }
     res.render('products/show', { product })
-})
 
-app.get('/products/:id/edit', async (req, res)=>{
+}))
+
+app.get('/products/:id/edit', wrapAsync(async (req, res, next)=>{
     const { id } = req.params;
     const product = await Product.findById(id)
-    res.render('products/edit', { product, categories })
-})
+    if(!product){
+        throw new AppError('Product not found', 404)
+     }
+    res.render('products/edit', { product, categories })}
+    
+))
 
-app.put('/products/:id', async (req, res)=>{
+app.put('/products/:id', wrapAsync(async (req, res, next)=>{
+    
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true})
     res.redirect(`/products/${product._id}`)
-})
+    
+}))
 
 app.delete('/products/:id', async (req, res)=> {
     const { id } = req.params;
@@ -84,8 +103,19 @@ app.delete('/products/:id', async (req, res)=> {
     res.redirect('/products')
 })
 
+const handleValidationErr = err => {
+    console.dir(err)
+    return new AppError(`Validation Failed...${err.message}`, 400)
+}
+
 app.use((err, req, res, next) =>{
-    const { status = 500, message = 'Something went wrong :('} = err
+    console.log(err.name)
+    if (err.name === 'ValidationError') err = handleValidationErr(err)
+    next(err)
+})
+
+app.use((err, req, res, next) =>{
+    const { status = 500, message = 'Something went wrong!!'} = err
     res.status(status).send(message)
 })
 
